7042abfea8dda2aef1cef63e55213e3a4099f032
LaroSelf <laroself1@gmail.com>
2016-03-12 17:36:39 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+
+public class Main {
+
+    public static char[] uniqueChars(String S){
+        char [] A = S.toCharArray();
+        StringBuilder SB = new StringBuilder();
+        for (char N: A ) {
+           if (S.indexOf(N, 0)==S.lastIndexOf(N, A.length)){
+               SB.append(N);  }
+        }
+        char [] res = new char[SB.length()];
+        SB.getChars(0,SB.length(),res,0);
+    return res;}
+
+    public static void main(String[] args) {
+        String a = "abracadabra";  
+      System.out.println(Main.uniqueChars(a));
+    }
+}
