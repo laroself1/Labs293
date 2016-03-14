051a1703b4b59103c45b659a99ed42451c8fd8ef
jref <aleksandrnvk@ukr.net>
2016-03-14 17:27:02 +0200

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@
 public class Main {
 
     public static char[] uniqueChars(String S){
-        char [] A = S.toCharArray();
+        char [] A = S.toCharArray();                   //don't use capital letters for variable names
         StringBuilder SB = new StringBuilder();
         for (char N: A ) {
            if (S.indexOf(N, 0)==S.lastIndexOf(N, A.length)){
